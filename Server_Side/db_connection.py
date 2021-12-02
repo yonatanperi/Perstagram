e28e6d0197615595c9096608832a6d2b3a12b98b
yonatanperi <yonatanperi333@gmail.com>
2021-12-02 17:38:27 +0200

    Fix hashing problem in the db.

--- Server_Side/db_connection.py
+++ Server_Side/db_connection.py
@@ -1,11 +1,13 @@
 import mysql.connector
 import pickle
+import hashlib
 
 
 class SQL:
     NAME_MAX_LENGTH = 50
     EMAIL_MAX_LENGTH = 100
     BIO_MAX_LENGTH = 500
+    SHA256_LENGTH = 64
 
     def __init__(self):
         """
@@ -34,25 +36,30 @@ class SQL:
         """
         self.cursor.execute(
             f"""CREATE TABLE IF NOT EXISTS users_info (
-                    username BIGINT NOT NULL PRIMARY KEY,
+                    username VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL PRIMARY KEY,
                     first_name VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL,
                     last_name VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL,
                     email VARCHAR({self.EMAIL_MAX_LENGTH}) NOT NULL)""")
 
         self.cursor.execute(
             f"""CREATE TABLE IF NOT EXISTS users_authentication (
-                    username BIGINT NOT NULL,
+                    username VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL,
                     FOREIGN KEY (username) REFERENCES users_info(username) ON DELETE CASCADE,
-                    password BIGINT NOT NULL,
+                    password VARCHAR({self.SHA256_LENGTH}) NOT NULL,
                     user_type VARCHAR(20) NOT NULL)""")
 
         self.cursor.execute(
             f"""CREATE TABLE IF NOT EXISTS users_profile (
-                    username BIGINT NOT NULL,
+                    username VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL,
                     FOREIGN KEY (username) REFERENCES users_info(username) ON DELETE CASCADE,
-                    bio VARCHAR({self.BIO_MAX_LENGTH}))""")
+                    bio VARCHAR({self.BIO_MAX_LENGTH}),
+                    last_post_date DATETIME,
+                    last_story_date DATETIME)""")
 
         self.db.commit()
+        self.register_admin()
+
+    def register_admin(self):
         self.register("yonatan", "7324545", "yonatan", "peri", "yonatanperi333@gmail.com", "admin")
 
     def register(self, username, password, first_name, last_name, email, user_type="client"):
@@ -64,9 +71,12 @@ class SQL:
                 False - the registration proses wasn't successful - something went wrong.
         """
 
+        # pre-checks to username and password
+        if not username.isalnum():  # has no special characters
+            return False
+
         # the username and password are hashed.
-        username = username.__hash__()
-        password = password.__hash__()
+        password = self.sha256(password)
 
         # check there isn't already a user with that username or the user entered to long values
         self.cursor.execute("SELECT * FROM users_authentication WHERE username = %s", (username,))
@@ -78,11 +88,10 @@ class SQL:
                             (username, first_name, last_name, email))
         self.cursor.execute("INSERT INTO users_authentication (username, password, user_type) VALUES (%s, %s, %s)",
                             (username, password, user_type))
-        self.cursor.execute("INSERT INTO users_profile (username, bio) VALUES (%s, %s)",
-                            (username, None))
+        self.cursor.execute("INSERT INTO users_profile (username) VALUES (%s)",
+                            (username,))
 
         # create new schema for the new user
-        username = "u" + str(username).replace("-", "_")  # if the hash is neg
 
         self.cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {username}")
         self.cursor.execute(
@@ -96,14 +105,16 @@ class SQL:
                     comment_id INT NOT NULL PRIMARY KEY,
                     post_id INT NOT NULL,
                     FOREIGN KEY (post_id) REFERENCES {username}.posts(post_id) ON DELETE CASCADE,
-                    username BIGINT NOT NULL,
+                    username VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL,
+                    FOREIGN KEY (username) REFERENCES perstagram.users_info(username) ON DELETE CASCADE,
                     comment VARCHAR({self.BIO_MAX_LENGTH}))""")
 
         self.cursor.execute(
             f"""CREATE TABLE IF NOT EXISTS {username}.likes (
                             post_id INT NOT NULL,
                             FOREIGN KEY (post_id) REFERENCES {username}.posts(post_id) ON DELETE CASCADE,
-                            username BIGINT NOT NULL)""")
+                            username VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL,
+                            FOREIGN KEY (username) REFERENCES perstagram.users_info(username) ON DELETE CASCADE)""")
 
         self.cursor.execute(
             f"""CREATE TABLE IF NOT EXISTS {username}.stories (
@@ -113,7 +124,8 @@ class SQL:
 
         self.cursor.execute(
             f"""CREATE TABLE IF NOT EXISTS {username}.interest_users (
-                                    username BIGINT NOT NULL PRIMARY KEY,
+                                    username VARCHAR({self.NAME_MAX_LENGTH}) NOT NULL,
+                                    FOREIGN KEY (username) REFERENCES perstagram.users_info(username) ON DELETE CASCADE,
                                     follows BOOLEAN,
                                     following BOOLEAN)""")
 
@@ -129,13 +141,13 @@ class SQL:
                 False - username and password doesn't match.
         """
         self.cursor.execute("SELECT * FROM users_authentication WHERE username = %s AND password = %s",
-                            (username.__hash__(), password.__hash__()))
+                            (username, self.sha256(password)))
         if self.cursor.fetchall():
             return True
         return False
 
     def get_user_type(self, username):
-        self.cursor.execute('SELECT user_type FROM users_authentication WHERE username = %s', (username.__hash__(),))
+        self.cursor.execute('SELECT user_type FROM users_authentication WHERE username = %s', (username,))
         return self.cursor.fetchall()[0][0]
 
     def execute_query(self, query, commit=False, get_results=False):
@@ -159,10 +171,10 @@ class SQL:
         :param image: PIL.Image object
         """
         # updating the sql - username schema
-        self.cursor.execute("SELECT post_id FROM {username}.posts")
+        self.cursor.execute("SELECT post_id FROM %s.posts", (username,))
         post_id = len(self.cursor.fetchall())
-        self.cursor.execute("INSERT INTO {username}.posts (post_id, byte_photo) VALUES (%s, %s)",
-                            (username.__hash__(), post_id, pickle.dumps(image)))
+        self.cursor.execute("INSERT INTO %s.posts (post_id, byte_photo) VALUES (%s, %s)",
+                            (username, post_id, pickle.dumps(image)))
         self.db.commit()
 
     def reset_db(self):
@@ -170,17 +182,23 @@ class SQL:
         delete all data from the default schema's tables,
         delete all user schemas.
         """
+
+        self.cursor.execute("SELECT username from users_info")
+        for schema in self.cursor.fetchall():
+            self.cursor.execute(f"DROP SCHEMA IF EXISTS {schema[0]}")
+
         self.cursor.execute("SHOW TABLES")
         for table in self.cursor.fetchall():
             self.cursor.execute(f"DELETE FROM {table[0]}")
 
-        self.cursor.execute("SHOW SCHEMAS")
-        for schema in self.cursor.fetchall():
-            if schema[0][0] == "u":
-                self.cursor.execute(f"DROP SCHEMA {schema[0]}")
+        self.db.commit()
 
-        self.create_tables()
+        self.register_admin()
+    
+    @staticmethod
+    def sha256(string):
+        return hashlib.sha224(string.encode()).hexdigest()
 
 
-"""s = SQL()
-s.reset_db()"""
+s = SQL()
+s.reset_db()
